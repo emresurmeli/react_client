4689bf2cc5e0de83c573fa513dd581c323442b99
Emre Surmeli <s.emre.s.8@gmail.com>
2015-06-13 16:30:31 -0700

    tests

--- tests/client/test.js
+++ tests/client/test.js
@@ -5,13 +5,13 @@ var greet = require('../../app/js/greet');
 var notes = require('../../app/js/notes');
 
 describe('greet module', function() {
-	it('should return a greeting', function() {
-		expect(greet()).to.eql('Hello world, from javascript');
-	});
+  it('should return a greeting', function() {
+    expect(greet()).to.eql('Hello world, from javascript');
+  });
 });
 
 describe('notes module', function() {
-	it('should return an explanation for the notes app', function() {
-		expect(notes()).to.eql('. The notes app lets you take notes and such, ya dig?');
-	});
+  it('should return an explanation for the notes app', function() {
+    expect(notes()).to.eql('. The notes app lets you take notes and such, ya dig?');
+  });
 });
